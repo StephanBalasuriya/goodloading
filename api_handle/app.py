44bef41bc3c69148fe9c9e90d83c0e62de3587ca
StephanBalasuriya <kanishkabalasuriya2002@gmail.com>
2026-06-19 10:45:53 +0000

    feat: update API port to 8002, allow additional local development origins, and add environment file ignores

--- api_handle/app.py
+++ api_handle/app.py
@@ -27,10 +27,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
+        "http://localhost:5175",
+        "http://127.0.0.1:5175",
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    
     allow_headers=["*"],
 )
 
